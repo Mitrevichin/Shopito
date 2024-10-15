c5de63c893620097fcbb9c4417640c6404b42a09
Petar Mitrevichin <pmitrevichin@gmail.com>
2024-10-15 12:54:00 +0300

    navbar created

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,11 +1,40 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { FaCartShopping } from 'react-icons/fa6';
 import styles from './Header.module.scss';
+
+const activeLink = ({ isActive }) => (isActive ? styles.active : '');
 
 function Header() {
   return (
     <header>
       <div className={styles.header}>
         <Logo />
+
+        <nav>
+          <ul>
+            <li>
+              <NavLink to='/shop' className={activeLink}>
+                Shop
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <div className={styles['header-right']}>
+          <span className={styles.links}>
+            <NavLink to='/login' className={activeLink}>
+              Login
+            </NavLink>
+            <NavLink to='/register' className={activeLink}>
+              Register
+            </NavLink>
+            <NavLink to='/order-history' className={activeLink}>
+              My Order
+            </NavLink>
+          </span>
+
+          <Cart />
+        </div>
       </div>
     </header>
   );
@@ -22,5 +51,17 @@ export function Logo() {
         </h2>
       </Link>
     </div>
+  );
+}
+
+export function Cart() {
+  return (
+    <span className={styles.cart}>
+      <Link to='/cart'>
+        Cart
+        <FaCartShopping size={20} />
+        <p>0</p>
+      </Link>
+    </span>
   );
 }
