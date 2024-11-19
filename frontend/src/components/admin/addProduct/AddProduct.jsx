6de34cd15d24bb058c226e59bec0d6a1fb618fc0
Petar Mitrevichin <pmitrevichin@gmail.com>
2024-11-19 17:53:47 +0200

    add name and category to the add product functionallity

--- frontend/src/components/admin/addProduct/AddProduct.jsx
+++ frontend/src/components/admin/addProduct/AddProduct.jsx
@@ -1,7 +1,56 @@
 import './AddProduct.scss';
+import Loader from '../../../components/loader/Loader';
+import { useDispatch, useSelector } from 'react-redux';
+import ProductForm from '../productForm/ProductForm';
+import { useEffect, useState } from 'react';
+import { getCategories } from '../../../redux/features/categoryAndBrand/categoryAndBrandSlice';
+
+const initialState = {
+  name: '',
+  category: '',
+  brand: '',
+  quantity: '',
+  color: '',
+  price: '',
+  regularPrice: '',
+};
 
 function AddProduct() {
-  return <div>Add</div>;
+  const [product, setProduct] = useState(initialState);
+  const { name, category, brand, quantity, color, price, regularPrice } =
+    product;
+  const { isLoading } = useSelector(state => state.product);
+  const { categories, brands } = useSelector(state => state.category);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch]);
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setProduct(prevProducts => ({ ...prevProducts, [name]: value }));
+  }
+  async function saveProduct(e) {
+    e.preventDefault();
+    console.log(product);
+  }
+
+  return (
+    <section>
+      {isLoading && <Loader />}
+      <div className='container'>
+        <h3 className='--mt'>Add New Product</h3>
+        <ProductForm
+          saveProduct={saveProduct}
+          product={product}
+          handleInputChange={handleInputChange}
+          categories={categories}
+          isEditing={false}
+        />
+      </div>
+    </section>
+  );
 }
 
 export default AddProduct;
--- frontend/src/components/admin/productForm/ProductForm.jsx
+++ frontend/src/components/admin/productForm/ProductForm.jsx
@@ -1,7 +1,56 @@
 import './ProductForm.scss';
+import Card from '../../card/Card';
 
-function ProductForm() {
-  return <div>Form</div>;
+function ProductForm({
+  saveProduct,
+  product,
+  handleInputChange,
+  categories,
+  isEditing,
+}) {
+  return (
+    <div className='add-product'>
+      <h3>UPLOAD WIDGET PLACEHOLDER</h3>
+
+      <Card>
+        <br />
+
+        <form onSubmit={saveProduct}>
+          <label>Product Name:</label>
+          <input
+            type='text'
+            name='name'
+            placeholder='Product name'
+            value={product?.name}
+            onChange={handleInputChange}
+          />
+          <label>Product Category:</label>
+          <select
+            name='category'
+            value={product?.category}
+            onChange={handleInputChange}
+          >
+            {isEditing ? (
+              <option value={product?.category}>{product?.category}</option>
+            ) : (
+              <option>Select Category</option>
+            )}
+
+            {categories.length > 0 &&
+              categories.map(cat => (
+                <option key={cat._id} value={cat.name}>
+                  {cat.name}
+                </option>
+              ))}
+          </select>
+
+          <div className='--my'>
+            <button className='--btn --btn-primary'>Save Product</button>
+          </div>
+        </form>
+      </Card>
+    </div>
+  );
 }
 
 export default ProductForm;
