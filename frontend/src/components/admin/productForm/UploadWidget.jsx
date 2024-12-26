c753b2630dd14e452aa336c0887e3d5246cd2e1d
Petar Mitrevichin <pmitrevichin@gmail.com>
2024-12-26 14:34:09 +0200

    upload images functionallity

--- frontend/src/components/admin/productForm/UploadWidget.jsx
+++ frontend/src/components/admin/productForm/UploadWidget.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import Card from '../../card/Card';
 import { AiOutlineCloudUpload } from 'react-icons/ai';
 import { FaTrash } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+
+const upload_preset = import.meta.env.VITE_UPLOAD_PRESET;
+const cloud_url = 'https://api.cloudinary.com/v1_1/dy1ppo3wm/image/upload';
 
 function UploadWidget({ files, setFiles }) {
   // State for showing the selected images to the user
@@ -37,6 +41,47 @@ function UploadWidget({ files, setFiles }) {
     URL.revokeObjectURL(image);
   }
 
+  function uploadImages() {
+    setUploading(true);
+
+    let imageUrls = [];
+    const formData = new FormData();
+
+    for (let i = 0; i < images.length; i++) {
+      let file = images[i];
+
+      formData.append('file', file);
+      formData.append('upload_preset', upload_preset);
+      formData.append('folder', 'shopitoApp');
+
+      fetch(cloud_url, {
+        method: 'POST',
+        body: formData,
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          imageUrls.push(data.secure_url);
+          setProgress(imageUrls.length);
+
+          if (imageUrls.length === images.length) {
+            setFiles(prevFiles => prevFiles.concat(imageUrls));
+            setUploading(false);
+            console.log(files);
+            toast.success('Image upload complete');
+            setImages([]);
+            setSelectedImages([]);
+            setProgress(0);
+          }
+        })
+        .catch(error => {
+          setUploading(false);
+          toast.error(error.message);
+          console.log(error);
+        });
+    }
+  }
+
   return (
     <div>
       <Card cardClass={'formcard group'}>
@@ -53,6 +98,25 @@ function UploadWidget({ files, setFiles }) {
           />
         </label>
         <br />
+
+        {selectedImages.length > 0 && selectedImages.length > 5 ? (
+          <p className='error'>
+            You can&apos;t upload more than 5 images! <br />
+            <span>
+              Please remove&nbsp;
+              {selectedImages.length - 5} of them
+            </span>
+          </p>
+        ) : (
+          <div className='--center-all'>
+            <button
+              className='--btn --btn-danger --btn-large'
+              onClick={uploadImages}
+            >
+              Upload Image
+            </button>
+          </div>
+        )}
 
         {/* View Selected Images */}
         <div className={selectedImages.length > 0 ? 'images' : ''}>
